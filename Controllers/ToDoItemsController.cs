2ba458f31e2d1e037100bcd0e254eef7777e6ff6
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-11-28 13:37:03 +0100

    asd

--- Controllers/ToDoItemsController.cs
+++ Controllers/ToDoItemsController.cs
@@ -38,7 +38,6 @@ public class ToDoItemsController : ControllerBase
     }
 
     // PUT: api/ToDoItems/5
-    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
     [HttpPut("{id}")]
     public async Task<IActionResult> PutToDoItem(long id, ToDoItemDTO toDoItemDTO)
     {
@@ -69,7 +68,6 @@ public class ToDoItemsController : ControllerBase
     }
 
     // POST: api/ToDoItems
-    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
     [HttpPost]
     public async Task<ActionResult<ToDoItemDTO>> PostToDoItem(ToDoItemDTO toDoItemDTO)
     {
